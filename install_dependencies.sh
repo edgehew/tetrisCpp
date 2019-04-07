ef2379a51be397e1407aa3cb7d64db7a34b9e88e
Matthew Edge <medge1@mail.csuchico.edu>
2019-04-07 01:07:56 -0700

    update to install script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
-# Run script as SUDO user because need to install sfml library
-pacman -Syy
-pacman -Sy libsfml-dev
+COMMAND=$1
+# Run script as SUDO user to install sfml library
+if [ "$COMMAND" = "pacman" ]; then
+    pacman -Syy
+    pacman -Sy libsfml-dev
+elif [ "$COMMAND" = "apt-get" ]; then
+    apt-get install libsfml-dev
+else
+    echo "Invalid Argument given"
+fi
+
